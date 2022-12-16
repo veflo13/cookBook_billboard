e48cea49740e1c373e57663dbed6e16793958bf0
Verónica Flores Gutiérrez <veflo13@gmail.com>
2022-12-16 15:40:00 +0100

    try slide

--- js/index.js
+++ js/index.js
@@ -14,9 +14,35 @@ fetchmycookbookjson().then(mycookbookjson=>{
         let title = mycookbookjson.mycookbook[index].titulo
         let ingredients = mycookbookjson.mycookbook[index].ingredientes
         let instructions = mycookbookjson.mycookbook[index].instrucciones
+        let img = mycookbookjson.mycookbook[index].img
+
 
         
-        cookBookSection.innerHTML += `${title}`
-    }
-})
+        cookBookSection.innerHTML += `
 
+        <div class="images">
+            <img class="img" src="${img}"></img> 
+        </div>
+        <h2>${id}.- ${title}</h2>
+        <div class="instructions">
+            <p>${instructions}</p>
+        </div> 
+        `
+        
+        console.log(instructions)
+            for(let j=0; j<ingredients.length; j++){
+                console.log(instructions)
+                
+                cookBookSection.innerHTML += `
+                <div class="container">
+                    <ul>
+                        <li>${ingredients[j]}</li>
+                    </ul> 
+                    
+                </div>
+                `
+            }
+            
+    }
+    console.log(mycookbookjson)
+})
